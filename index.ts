7dcaf65795cd4960686fc85abb9a6430f61fc144
medmor <elmadioune@gmail.com>
2020-07-25 14:25:48 +0100

    2 files modified

--- index.ts
+++ index.ts
@@ -2,13 +2,8 @@
 import './style.css';
 
 import * as pixi from "pixi.js"
-import {default as sound} from "pixi-sound"
 
-pixi["soun" + "d"] = sound
 
-pixi.sound.Sound.from("./sounds")
-
-console.log(pixi.sound)
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
